54e4a3b84671848e215f0e4f84e1c62aa1def2e2
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-04-24 11:23:05 +0500

    Revert "E2e testing"

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,13 +14,6 @@ export default defineConfig({
 	chromeWebSecurity: false,
 	e2e: {
 		setupNodeEvents(on, config) {
-			on('task', {
-				log(message) {
-				  console.log(message)
-		
-				  return null
-				},
-			  })
 			// eslint-disable-next-line no-param-reassign
 			config = cypressBrowserPermissionsPlugin(on, config);
 			allureWriter(on, config);
