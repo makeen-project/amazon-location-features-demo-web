281cb470f4d1ffdefe7858a20c01089759121af4
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-07-10 15:54:32 +0500

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
 /* SPDX-License-Identifier: MIT-0 */
 
-// import ReactDOM from "react-dom/client";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
