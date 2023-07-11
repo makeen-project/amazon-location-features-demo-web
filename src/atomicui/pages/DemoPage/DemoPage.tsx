1a67eabb79388b9e5ce7faf95f489aafd2bb83ca
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2023-07-11 15:13:28 +0500

    testing

--- src/atomicui/pages/DemoPage/DemoPage.tsx
+++ src/atomicui/pages/DemoPage/DemoPage.tsx
@@ -118,14 +118,12 @@ const DemoPage: React.FC = () => {
 	const { attachPolicy } = useAwsIot();
 	const {
 		mapProvider: currentMapProvider,
+		mapStyle: currentMapStyle,
+		currentLocationData,
 		setMapProvider,
 		setMapStyle,
 		isCurrentLocationDisabled,
-		setIsCurrentLocationDisabled
-	} = useAmplifyMap();
-	const {
-		mapStyle: currentMapStyle,
-		currentLocationData,
+		setIsCurrentLocationDisabled,
 		setCurrentLocation,
 		viewpoint,
 		setViewpoint,
