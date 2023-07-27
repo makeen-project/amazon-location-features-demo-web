bed7c4a94e7642272d911ef2f006366434b21647
NihaalAftab-Makeen <Nihaal.Aftab@makeen.io>
2023-07-28 01:14:38 +0500

    fix pageViewDuration

--- src/hooks/useRecordViewPage.ts
+++ src/hooks/useRecordViewPage.ts
@@ -13,7 +13,7 @@ const useRecordViewPage = (pageName: string) => {
 		record([{ EventType: EventTypeEnum.VIEW_PAGE, Attributes: { path, pageName } }]);
 
 		return () => {
-			const pageViewDuration = String((new Date().getTime() - pageViewDateTime.getTime()) / 1000 / 60);
+			const pageViewDuration = String(((new Date().getTime() - pageViewDateTime.getTime()) / 1000 / 60).toFixed(1));
 			record([{ EventType: EventTypeEnum.LEAVE_PAGE, Attributes: { path, pageName, pageViewDuration } }]);
 		};
 	}, [pageName]);
