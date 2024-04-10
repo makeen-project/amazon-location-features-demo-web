156ecb0ec1f4e1babfdd0f384cad96252444f2c1
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-04-10 09:19:52 +0500

    in progress

--- src/hooks/useApiPlaygroundList.ts
+++ src/hooks/useApiPlaygroundList.ts
@@ -1,3 +1,26 @@
-const useApiPlaygroundList = () => {};
+import { useState } from "react";
+
+import { appConfig } from "@demo/core/constants";
+import { EventTypeEnum } from "@demo/types/Enums";
+import { debounce, record } from "@demo/utils";
+import { useTranslation } from "react-i18next";
+
+const recordFilterEvent = debounce(
+	(recordAttributes: { [key: string]: string }) =>
+		record([{ EventType: EventTypeEnum.API_PLAYGROUND_FILTERS_APPLIED, Attributes: recordAttributes }]),
+	5000
+);
+const {
+	ENV: { API_PLAYGROUND_LIST_FILENAME }
+} = appConfig;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type TODO = any;
+
+const useApiPlaygroundList = () => {
+	const [apisList, setApisList] = useState<TODO | null>(null);
+	const [isLoading, setLoading] = useState(false);
+	const { t, i18n } = useTranslation();
+};
 
 export default useApiPlaygroundList;
