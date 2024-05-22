382127c7254ce59d8f0bee0302f67015e2e929ad
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-05-22 17:07:13 +0500

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,4 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./locales/i18n";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
