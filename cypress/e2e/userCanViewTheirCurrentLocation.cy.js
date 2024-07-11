36a5d11eabad559911752c134daf057a7d9e4de6
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-07-12 04:43:17 +0500

    update

--- cypress/e2e/userCanViewTheirCurrentLocation.cy.js
+++
@@ -1,9 +0,0 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
-/* SPDX-License-Identifier: MIT-0 */
-
-describe("Current location", () => {
-	it("CL-001 - should allow user to see their current location", { scrollBehavior: false }, () => {
-		cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
-		cy.get('[class="mapboxgl-user-location-dot mapboxgl-marker mapboxgl-marker-anchor-center"]').should("be.visible");
-	});
-});
