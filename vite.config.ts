a064f3ccf3462074d31c16c73756a75861074f41
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-07-14 00:30:18 +0500

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import eslint from "vite-plugin-eslint";
 import Inspect from "vite-plugin-inspect";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
 	return {
+		publicDir: env.VITE_PUBLIC_DIR || resolve(__dirname, "./public"),
 		plugins: [
 			react(),
 			svgr(),
