9de032111b34c26a25708c8a2dd25ac2f1188f38
Ahmad Azizi <91204996+its-aazizi@users.noreply.github.com>
2024-07-14 23:19:36 +0500

    updates

--- cypress/e2e/userIsAbleToUseGeofence.cy.js
+++ cypress/e2e/userIsAbleToUseGeofence.cy.js
@@ -1,28 +1,34 @@
 /* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. */
 /* SPDX-License-Identifier: MIT-0 */
 
+import { faker } from "@faker-js/faker";
+
 import { Viewport } from "../support/constants";
 
-describe("Connecting and Disconnecting AWS account", () => {
+describe("Geofence", () => {
 	context("Desktop view", () => {
+		const geofenceName = faker.random.word();
+
 		beforeEach(() => {
 			cy.visitDomain(`${Cypress.env("WEB_DOMAIN")}/demo`);
 			cy.connectAwsAccount(Viewport.DESKTOP);
 		});
 
-		it("CDAA-001 - should allow user to connect, sign-in, sign-out and disconnect AWS account from sidebar", () => {
-			cy.signOutAndDisconnectFromAwsAccount(Viewport.DESKTOP);
+		it("GF-001 - should allow user to add, edit and delete geofence", () => {
+			cy.addEditAndDeleteGeofence(Viewport.DESKTOP, geofenceName);
 		});
 	});
 
 	context("Responsive view", () => {
+		const geofenceName = faker.random.word();
+
 		beforeEach(() => {
 			cy.visitDomainInResponsiveView(`${Cypress.env("WEB_DOMAIN")}/demo`);
 			cy.connectAwsAccount(Viewport.RESPONSIVE);
 		});
 
-		it("CDAA-002 - should allow user to connect, sign-in, sign-out and disconnect AWS account from sidebar", () => {
-			cy.signOutAndDisconnectFromAwsAccount(Viewport.RESPONSIVE);
+		it("GF-002 - should allow user to add, edit and delete geofence", () => {
+			cy.addEditAndDeleteGeofence(Viewport.RESPONSIVE, geofenceName);
 		});
 	});
 });
